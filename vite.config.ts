59c2cf32c59671322a59d44054b7f18613e7b91b
Rishikesh pandey <rishikeshpandey1999@gmail.com>
2025-12-05 17:04:42 +0530

    Updated website content

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This ensures assets work with relative paths on GitHub Pages
+  base: '/Destination-kashi2', // This ensures assets work with relative paths on GitHub Pages
 });
